64a242fab9b74dd7d0e4535fffcd53f90797aee8
Patrick Gross <pgross41@gmail.com>
2019-09-16 00:37:03 -0500

    Stub in API routes

---
+++ src/controllers/snapshotController.ts
@@ -0,0 +1,5 @@
+import express = require("express");
+
+export default (req: express.Request, res: express.Response) => {
+    res.send({testValue: "snapshot"});
+};
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
+// tslint:disable:no-console
 import express from "express";
-const app = express();
+import path from "path";
+import configController from "./controllers/configController";
+import snapshotController from "./controllers/snapshotController";
+import streamMjpegController from "./controllers/streamMjpegController";
+
 const port = process.env.PORT || 5000;
+const app = express();
+const api = express.Router();
 
-// console.log that your server is up and running
-// tslint:disable-next-line:no-console
+// Host static client code
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+// Log requests
+api.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next(); });
+
+// Define routes
+api.use("/config", configController);
+api.use("/snapshot/:channelNumber", snapshotController);
+api.use("/stream/mjpeg/:channelNumber", streamMjpegController);
+app.use("/api", api);
+
+// Listen for requests
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get("/ping", (req, res) => {
-    // tslint:disable-next-line:no-console
-    console.log("request");
-    res.send({ express: "pong" });
-});
